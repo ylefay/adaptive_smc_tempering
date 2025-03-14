3a945f74ef08fab1ef37b741baec7d8d80a557e9
Yvann Le Fay <yvann.lefay@ensae.fr>
2025-03-14 18:23:03 +0100

    Ready to run experiments.

--- adaptive_smc/experiments_bis/GLOBAL.py
+++ adaptive_smc/experiments_bis/GLOBAL.py
@@ -1,7 +1,7 @@
 import jax
 
 jax.config.update("jax_enable_x64", False)
-jax.config.update("jax_disable_jit", False)
+jax.config.update("jax_disable_jit", True)
 # jax.config.update("default_device", "cpu")
 
 OP_key = jax.random.PRNGKey(0)
