Implemented apply_vmap_batch to fix the memory overflow issue

adaptive_smc/experiments_bis/rw_adaptive_fim_vs_cov/rw_adaptive_fim_vs_cov.py
@@ -14,8 +14,8 @@ from adaptive_smc.smc_bis import GenericAdaptiveWasteFreeTemperingSMC
 OP_key = jax.random.PRNGKey(0)
 _, key = jax.random.split(OP_key)
 
-rho_grid = jnp.linspace(0, 1, 25)
-tau_grid = jnp.linspace(0.01, 2, 25)
+tau_grid = jnp.linspace(2, 4, 25)
+
 
 def default_title(prefix=''):
     now = datetime.now()
@@ -24,7 +24,7 @@ def default_title(prefix=''):
     return output_path
 
 
-def experiment_ar(config, keys):
+def experiment_adaptive_cov_rw(config, keys):
     dim = config.get('dim')
 
     target_ess = config.get('target_ess')
@@ -36,53 +36,7 @@ def experiment_ar(config, keys):
     tempering_length = config.get('tempering_length', 10 + dim)
     my_tempering_sequence = jnp.linspace(0, 1, tempering_length)
 
-    params_optimization_method = {"grid": rho_grid}
-    # params_optimization_method = {"minmax": [0.1, 10.], "interval": [-5., 5.], "n_iter":4}
-
-    loglikelihood_fn, base_measure_sampler, logbase_density_fn = construct_my_prior_and_target(config)
-    tempering_length = config.get('tempering_length', 10 + dim)
-    my_tempering_sequence = jnp.linspace(0, 1, tempering_length)
-
-    init_param = jnp.array([0])
-    config.update(
-        {"optimization_method": optimization_method_str, "params_optimization_method": params_optimization_method,
-         "proposal": "build_build_autoregressive_gaussian_proposal",
-         "tempering_sequence": my_tempering_sequence,
-         "init_param": init_param})
-
-    my_proposal = getattr(proposals, config['proposal'])(jnp.zeros(dim), jnp.eye(dim))
-    if config['optimization_method']:
-        optimization_method = getattr(optimise, config['optimization_method'])(**params_optimization_method)
-    else:
-        optimization_method = None
-
-    smc = GenericAdaptiveWasteFreeTemperingSMC(logbase_density_fn, base_measure_sampler, loglikelihood_fn,
-                                               my_proposal, optimization_method, grid_criteria=params_optimization_method['grid'],
-                                               batch_size_criteria = 500)
-
-    @jax.vmap
-    def wrapper_smc(key):
-        return smc.sample(key, num_parallel_chain, num_mcmc_steps, init_param, my_tempering_sequence, target_ess)
-
-    res = wrapper_smc(keys)
-    save(res, config, config.get('OUTPUT_PATH') + default_title(config.get('prefix')))
-
-
-def experiment_rw(config, keys):
-    dim = config.get('dim')
-
-    target_ess = config.get('target_ess')
-    num_parallel_chain = config.get('num_parallel_chain')
-    num_mcmc_steps = config.get('num_mcmc_steps')
-
-    optimization_method_str = "make_optimize_within_a_fixed_grid"
-
-    tempering_length = config.get('tempering_length', 10 + dim)
-    my_tempering_sequence = jnp.linspace(0, 1, tempering_length)
-
-    scaltedparameter_grid = tau_grid * jnp.sqrt(dim)
-
-    params_optimization_method = {"grid": scaltedparameter_grid}
+    params_optimization_method = {"grid": tau_grid}
 
     loglikelihood_fn, base_measure_sampler, logbase_density_fn = construct_my_prior_and_target(config)
     tempering_length = config.get('tempering_length', 10 + dim)
@@ -91,19 +45,19 @@ def experiment_rw(config, keys):
     init_param = jnp.array([2.38])
     config.update(
         {"optimization_method": optimization_method_str, "params_optimization_method": params_optimization_method,
-         "proposal": "build_build_gaussian_rw_proposal",
+         "proposal": "build_gaussian_rwmh_cov_proposal_gamma",
          "tempering_sequence": my_tempering_sequence,
          "init_param": init_param})
 
-    my_proposal = getattr(proposals, config['proposal'])(jnp.eye(dim))
+    my_proposal = getattr(proposals, config['proposal'])
     if config['optimization_method']:
         optimization_method = getattr(optimise, config['optimization_method'])(**params_optimization_method)
     else:
         optimization_method = None
 
     smc = GenericAdaptiveWasteFreeTemperingSMC(logbase_density_fn, base_measure_sampler, loglikelihood_fn,
-                                               my_proposal, optimization_method, grid_criteria=params_optimization_method['grid'],
-                                               batch_size_criteria = 500)
+                                               my_proposal, optimization_method,
+                                               grid_criteria=params_optimization_method['grid'])
 
     @jax.vmap
     def wrapper_smc(key):
@@ -113,8 +67,7 @@ def experiment_rw(config, keys):
     save(res, config, config.get('OUTPUT_PATH') + default_title(config.get('prefix')))
 
 
-
-def experiment_uncoupled_ar_rw(config, keys):
+def experiment_adaptive_invfim_rw(config, keys):
     dim = config.get('dim')
 
     tempering_length = config.get('tempering_length', 10 + dim)
@@ -123,22 +76,22 @@ def experiment_uncoupled_ar_rw(config, keys):
     loglikelihood_fn, base_measure_sampler, logbase_density_fn = construct_my_prior_and_target(config)
 
     optimization_method_str = "make_optimize_within_a_fixed_grid"
-    params_grid = jnp.array([[x, y] for x in rho_grid for y in tau_grid])
-    params_optimization_method = {"grid": params_grid}
+    params_optimization_method = {"grid": tau_grid}
+
 
     num_parallel_chain = config.get('num_parallel_chain')
     num_mcmc_steps = config.get('num_mcmc_steps')
     target_ess = config.get('target_ess')
 
-    init_param = jnp.array([0., 1.])
+    init_param = jnp.array([2.38])
 
     config.update(
         {"optimization_method": optimization_method_str, "params_optimization_method": params_optimization_method,
-         "proposal": "build_build_uncoupled_autoregressive_gaussian_proposal",
+         "proposal": "build_gaussian_rwmh_invfim_proposal_gamma",
          "tempering_sequence": my_tempering_sequence,
          "init_param": init_param})
 
-    my_proposal = getattr(proposals, config['proposal'])(jnp.zeros(dim), jnp.eye(dim))
+    my_proposal = getattr(proposals, config['proposal'])
 
     if config['optimization_method']:
         optimization_method = getattr(optimise, config['optimization_method'])(**params_optimization_method)
@@ -146,7 +99,7 @@ def experiment_uncoupled_ar_rw(config, keys):
         optimization_method = None
 
     smc = GenericAdaptiveWasteFreeTemperingSMC(logbase_density_fn, base_measure_sampler, loglikelihood_fn,
-                                               my_proposal, optimization_method, grid_criteria=params_grid, batch_size_criteria=500)
+                                               my_proposal, optimization_method, grid_criteria=params_optimization_method['grid'])
 
     @jax.vmap
     def wrapper_smc(key):
@@ -155,8 +108,9 @@ def experiment_uncoupled_ar_rw(config, keys):
     res = wrapper_smc(keys)
     save(res, config, config.get('OUTPUT_PATH') + default_title(config['prefix']))
 
+
 if __name__ == "__main__":
-    yaml_file = "g_ar_rw_uncoupled.yaml"
+    yaml_file = "rw_adaptive_fim_vs_cov.yaml"
     with open(yaml_file, "r") as file:
         y_config = yaml.load(file, Loader=yaml.FullLoader)
 
@@ -168,6 +122,5 @@ if __name__ == "__main__":
             all_keys = jax.vmap(lambda k: jax.random.split(k, n_chains))(seq_keys)
             _, key = jax.random.split(seq_keys.at[-1].get())
             for keys in all_keys:
-                experiment_ar(config, keys)
-                experiment_rw(config, keys)
-                experiment_uncoupled_ar_rw(config, keys)
+                experiment_adaptive_cov_rw(config, keys)
+                experiment_adaptive_invfim_rw(config, keys)

adaptive_smc/proposals/rw.py
@@ -2,7 +2,7 @@ import jax
 import jax.numpy as jnp
 from jax.typing import ArrayLike
 
-from adaptive_smc.estimates import cov_estimate, cov_increment_estimate, estimate_I
+from adaptive_smc.estimates import inverse_FIM_gaussian_approx, cov_estimate, cov_increment_estimate, estimate_I
 from adaptive_smc.smc_types import LogDensity, SMCStatebis, ProposalBuilder
 
 __all__ = [
@@ -64,6 +64,32 @@ def build_gaussian_rwmh_cov_proposal_gamma(state: SMCStatebis, _: LogDensity, __
 
     return gaussian_rwmh_cov_log_proposal, gaussian_rwmh_sampler, jnp.empty(1)
 
+
+def build_gaussian_rwmh_invfim_proposal_gamma(state: SMCStatebis, log_tgt_density_fn: LogDensity, __: LogDensity, i: int):
+    """
+        Same as build_gaussian_rwmh_cov_proposal with gamma**2/dim in front of the covariance matrix
+        """
+    gamma = state.mh_proposal_parameters.at[i - 1].get()
+    particles = state.particles
+    dim = particles.shape[-1]
+    log_weights = state.log_weights
+    optimal_scale = gamma ** 2 / dim
+
+    def fun_to_be_called_if_i_greater_than_one():
+        r"""
+        Compute the inverse FIM estimate of \pi_{t-1} given t\geq 1
+        """
+        particles_at_i_minus_one = particles.at[i - 1].get().reshape(-1, particles.shape[-1])
+        log_weights_at_i_minus_one = log_weights.at[i - 1].get().reshape(-1, )
+        weights_at_i_minus_one = jnp.exp(log_weights_at_i_minus_one)
+        invFim_hat, _ = inverse_FIM_gaussian_approx(particles_at_i_minus_one, weights_at_i_minus_one)
+        return invFim_hat
+
+    C = optimal_scale * fun_to_be_called_if_i_greater_than_one()
+
+    gaussian_rwmh_cov_log_proposal, gaussian_rwmh_sampler, _ = build_gaussian_rw_proposal(C)
+
+    return gaussian_rwmh_cov_log_proposal, gaussian_rwmh_sampler, jnp.empty(1)
 
 def build_gaussian_rwmh_proposal_with_nicolas_cov_estimate(state: SMCStatebis, _: LogDensity,
                                                            log_likelihood_fn: LogDensity, i: int):

adaptive_smc/utils.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import jax
 import jax.random
@@ -26,3 +26,18 @@ def log_ess(delta: float, log_weights: Array) -> float:
     log_ess_scaled = log_ess - jnp.log(N_particles)
     return log_ess_scaled
 
+
+
+def vec(X: ArrayLike) -> Array:
+    """
+    Vectorization of a matrix.
+    """
+    return X.reshape(-1, order='F')
+
+def unvec(vecX: ArrayLike, shape=Optional[Tuple[int, int]]) -> Array:
+    """
+    Invert the previous operation
+    """
+    if shape is None:
+        shape = (int(vecX.shape[0] ** 0.5), int(vecX.shape[0] ** 0.5))
+    return vecX.reshape(shape, order='F')