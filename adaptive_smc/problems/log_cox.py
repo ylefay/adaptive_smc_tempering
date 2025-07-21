6754070d1de424351836b039eb7679d654022e24
Yvann Le Fay <yvann.lefay@ensae.fr>
2025-07-21 22:22:50 +0200

    minor modifications.

--- adaptive_smc/problems/log_cox.py
+++ adaptive_smc/problems/log_cox.py
@@ -1,4 +1,3 @@
-import jax
 import jax.numpy as jnp
 
 
@@ -14,31 +13,9 @@ def get_log_likelihood_fn(y, m):
     return log_likelihood_fn
 
 
-def make_buccholz_chopin_log_likelihood_fn(y):
-    """
-    See Section 4.5 from Adaptive tuning of hamiltonian monte carlo within sequential monte carlo
-    by Buchholz and Chopin (2021).
-    """
-    d = y.shape[0] if y.ndims > 1 else int(jnp.sqrt(y.shape[0]))
-    m = 1 / d ** 2
-
-    def delta_fn(j, jp, k, kp):
-        return jnp.sqrt((j - jp) ** 2 + (k - kp) ** 2)
-
-    indices = jnp.arange(1, d + 1)
-    j = indices[:, jnp.newaxis, jnp.newaxis, jnp.newaxis]
-    jp = indices[jnp.newaxis, :, jnp.newaxis, jnp.newaxis]
-    k = indices[jnp.newaxis, jnp.newaxis, :, jnp.newaxis]
-    kp = indices[jnp.newaxis, jnp.newaxis, jnp.newaxis, :]
-
-    delta = delta_fn(j, jp, k, kp).reshape(
-        (d ** 2, d ** 2))
-    Sigma = sigmasq * jnp.exp(-delta / (d * beta))
-    return get_log_likelihood_fn(y, m)
-
-
-def construct_my_prior_and_target(y):
+def construct_target(y):
     """"We hardcode the config.
+    Return the log likelihood and log target function
     """
     sigmasq = 1.91
     beta = 1 / 33
@@ -60,15 +37,13 @@ def construct_my_prior_and_target(y):
     Sigma = sigmasq * jnp.exp(-delta / (d * beta))
     precision = jnp.linalg.inv(Sigma)
 
-    def base_measure_sampler(key):
-        return jax.random.multivariate_normal(key, mu, Sigma)
-
-    def logbase_density_fn(x):
+    def log_prior(x):
         return -0.5 * (x - mu).T @ precision @ (x - mu)
 
     loglikelihood_fn = get_log_likelihood_fn(y, m)
 
-    return loglikelihood_fn, base_measure_sampler, logbase_density_fn
+    log_target_density = lambda x: loglikelihood_fn(x) + log_prior(x)
+    return loglikelihood_fn, log_target_density
 
 
 if __name__ == "__main__":
